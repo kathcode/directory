b36f829948c5ee3f2bfa63c7c8f491fcd29c436f
mountechcorp <mountechcorp@gmail.com>
2025-06-03 20:10:47 -0400

    add new contact

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import {
   Box,
+  Button,
+  Modal,
   Paper,
   Table,
   TableBody,
@@ -17,8 +19,23 @@ import ComputerIcon from "@mui/icons-material/Computer";
 import InstagramIcon from "@mui/icons-material/Instagram";
 import FacebookIcon from "@mui/icons-material/Facebook";
 
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
+
 function App() {
   const [search, setSearch] = useState("");
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   const filteredRows = directory.filter(
     (row) =>
@@ -43,6 +60,9 @@ function App() {
         Milton Latinas directory
       </Typography>
       <Paper sx={{ padding: 2 }}>
+        <Button variant="contained" onClick={handleOpen}>
+          Add new contact
+        </Button>
         <TextField
           label="Search"
           variant="outlined"
@@ -87,6 +107,30 @@ function App() {
           </Table>
         </TableContainer>
       </Paper>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            To add a new contact to the directory you need to send a WhatsApp to
+            the number <strong>+1 647 782 97 82</strong> with the following
+            information:
+          </Typography>
+          <ul>
+            <li>Name</li>
+            <li>Phone number</li>
+            <li>Kind of service</li>
+            <li>Social media</li>
+          </ul>
+          <Button sx={{ mt: 4 }} variant="contained" onClick={handleClose}>
+            Close
+          </Button>
+        </Box>
+      </Modal>
     </>
   );
 }
